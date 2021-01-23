07563c0929601c52cc56bc0b83cb19c9d8b82bf3
Luís Tavares <luistavares10@outlook.pt>
2021-01-23 16:44:25 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-<<<<<<< HEAD
 
 apt-get update
 apt-get install -y git
@@ -11,14 +10,5 @@ sh $DIR/requirements/system_requirements.sh
 pip3 install -U -r $DIR/requirements/py_requirements.txt
 
 export MAST_PATH=$DIR/src/main.py
-=======
-
-apt-get update
-apt-get install -y git
-
-sh $DIR/requirements/system_requirements.sh
-
-pip3 install -U -r $DIR/requirements/py_requirements.txt
->>>>>>> 8c1bb0800e47a607f5a3b19a2b2b5e832637508b
 
 echo 'alias mast="python3 '$DIR'/src/main.py"' >> ~/.bashrc
